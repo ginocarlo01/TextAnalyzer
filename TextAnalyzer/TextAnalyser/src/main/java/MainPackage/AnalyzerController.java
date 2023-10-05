f3dea0ed6b13ed9466644eac80161afd55134f20
Gino Grippo <gino.carlo02@gmail.com>
2023-10-05 11:32:54 -0300

    AnalyzerWriter updated

--- TextAnalyzer/TextAnalyser/src/main/java/MainPackage/AnalyzerController.java
+++ TextAnalyzer/TextAnalyser/src/main/java/MainPackage/AnalyzerController.java
@@ -21,7 +21,7 @@ public class AnalyzerController {
     }
 
     /**
-     * Processes the text files, including reading and generating adjacency lists.
+     * Processes the text files, including reading, generating adjacency lists, and writing to CSV files.
      */
     public void processFiles() {
         AnalyzerReader reader = new AnalyzerReader();
@@ -31,9 +31,13 @@ public class AnalyzerController {
             try {
                 // Read and process the file
                 Map<String, String> adjacencyList = reader.readFile(fileName);
+                System.out.println("Adjacency List for File: " + fileName);
+                for (Map.Entry<String, String> entry : adjacencyList.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
 
-                // Process the adjacency list and write the results
-                //writer.writeAdjacencyList(adjacencyList, fileName + ".csv");
+                // Write the adjacency list to a CSV file in the same directory as the input file
+                writer.writeAdjacencyList(adjacencyList, fileName);
             } catch (IOException e) {
                 System.err.println("Error processing file " + fileName + ": " + e.getMessage());
             }
--- TextAnalyzer/TextAnalyser/src/main/java/MainPackage/AnalyzerWriter.java
+++ TextAnalyzer/TextAnalyser/src/main/java/MainPackage/AnalyzerWriter.java
@@ -1,4 +1,35 @@
 package MainPackage;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * The AnalyzerWriter class handles writing the adjacency list to a CSV file.
+ */
 public class AnalyzerWriter {
+
+    /**
+     * Writes the adjacency list to a CSV file in the same directory as the input file.
+     *
+     * @param adjacencyList The adjacency list to be written.
+     * @param inputFilePath The path of the input file.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
+    public void writeAdjacencyList(Map<String, String> adjacencyList, String inputFilePath) throws IOException {
+        // Get the directory of the input file
+        File inputFile = new File(inputFilePath);
+        String outputFileName = inputFile.getParent() + File.separator + "output.csv";
+
+        try (FileWriter writer = new FileWriter(outputFileName)) {
+            for (Map.Entry<String, String> entry : adjacencyList.entrySet()) {
+                String node = entry.getKey();
+                String destinations = entry.getValue();
+
+                // Write the node and its destinations to the CSV file
+                writer.write(node + ": " + destinations + "\n");
+            }
+        }
+    }
 }
